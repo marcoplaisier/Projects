6e95bb97c8c3e7e914fb87b8b95267bd241d60a5
Marco Plaisier <m.plaisier@gmail.com>
2013-07-10 20:06:43 +0200

    Reset projects

--- Numbers/pi.py
+++ Numbers/pi.py
@@ -1,7 +1,31 @@
-# Find PI to the Nth Digit
-
+from decimal import Decimal, getcontext
 import math
 
-digits = raw_input('Enter number of digits to round PI to: ')
+__author__ = 'marco'
 
-print ('{0:.%df}' % min(20, int(digits))).format(math.pi) # nested string formatting
+def pi_generator():
+    #http://en.wikipedia.org/wiki/Gauss%E2%80%93Legendre_algorithm
+    getcontext().prec = 240
+    a = Decimal(1.0)
+    b = Decimal(1.0 / math.sqrt(2))
+    t = Decimal(1.0/4)
+    p = Decimal(1)
+
+    while True:
+        an = (a + b) / 2
+        bn = (a * b).sqrt()
+        tn = t - p * (a - an)*(a - an)
+        pn = 2*p
+
+        pi = ((an + bn)*(an + bn)) / (4*tn)
+        yield pi
+        a, b, t, p = an, bn, tn, pn
+
+if __name__ == '__main__':
+    pigen = pi_generator()
+    for i in range(100):
+        print(next(pigen))
+
+#3.140
+#3.14159264
+#3.1415926535897932382
